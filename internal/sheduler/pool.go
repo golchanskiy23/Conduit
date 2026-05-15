ad188e79fbc42c7e8ac4dd245521b240394f621a
max23 <golchanskiy23@gmail.com>
2026-05-15 07:22:54 +0300

    Change WorkerPool definition and fix Worker and Shutdown functions

--- internal/sheduler/pool.go
+++ internal/sheduler/pool.go
@@ -4,44 +4,67 @@ import (
 	"context"
 	"sync"
 	"time"
+	"fmt"
 )
 
+type WorkerPoolConfig struct {
+    BufferSize int
+    JobTimeout time.Duration
+}
+
+// добавить интерфейс, чтобы в пушить любой пул в scheduer constructor
 type WorkerPool struct{
 	Jobs chan *Item
 	Wg sync.WaitGroup
+	Cfg WorkerPoolConfig
+
 	onDone func(string)
-	// это какая-то функция выполнения воркера
+    onError func(string, error)
 	toExecution func(context.Context, *Item) error
 }
 
-func NewWorkerPool(size int, onDone func(string)) *WorkerPool{
-	return &WorkerPool{
-		Jobs: make(chan *Item, size),
-		onDone: onDone,
-	}
+func newWorkerPool(cfg WorkerPoolConfig, execute func(context.Context, *Item) error, onDone func(string), onError func(string, error)) *WorkerPool {
+    return &WorkerPool{
+        Jobs:    make(chan *Item, cfg.BufferSize),
+        Cfg:     cfg,
+        toExecution: execute,
+        onDone:  onDone,
+        onError: onError,
+    }
 }
 
 func (pool *WorkerPool) Worker(ctx context.Context){
 	defer pool.Wg.Done()
 	for job := range pool.Jobs{
-		// потенциальное время работы одной job
-		parent, cancel := context.WithTimeout(ctx, 1*time.Second)
-		defer cancel() // освобождение ресурсов таймера
-		err := pool.toExecution(parent, job) // может какой-то ответ
-		if err != nil{
-			// выводим ошибку или прокидываем выше
-		}
-		pool.onDone(job.JobID)
+		func() {
+            jobCtx, cancel := context.WithTimeout(ctx, pool.Cfg.JobTimeout)
+            defer cancel()
+            if err := pool.toExecution(jobCtx, job); err != nil {
+                pool.onError(job.JobID, err)
+            }
+            pool.onDone(job.JobID)
+        }()
 	}
 }
 
-// обработка "ошибок" вроде блокировки на полной очереди
 func (pool *WorkerPool) Execute(job *Item){
 	pool.Jobs <- job
 }
 
-// возвращать ошибку - ?
-func (pool *WorkerPool) Shutdown(){
+func (pool *WorkerPool) Shutdown(ctx context.Context) error{
 	close(pool.Jobs)
-	pool.Wg.Wait()
+	done := make(chan struct{})
+
+	// зачем передавать параметры, если анонимной функции видно всё в этой функции?
+	go func(){
+		pool.Wg.Wait()
+		close(done)
+	}()
+
+	select{
+	case <-done:
+		return nil
+	case <- ctx.Done():
+		return fmt.Errorf("worker pool timeout shutdown: %w", ctx.Err())
+	}
 }
