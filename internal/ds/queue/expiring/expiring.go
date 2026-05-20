438f2a0ffadd1b2d3f7ffffeae030e568be5cb2b
max23 <golchanskiy23@gmail.com>
2026-05-20 22:21:37 +0300

    Expiring heap implementation

--- internal/ds/queue/expiring/expiring.go
+++ internal/ds/queue/expiring/expiring.go
@@ -14,10 +14,27 @@ type ExpiryHeap []*ExpiryItem
 
 func (h ExpiryHeap) Len() int{return 0}
 
-func (h ExpiryHeap) Less(i,j int) bool{return false}
+func (h ExpiryHeap) Less(i,j int) bool{return h[i].ExpiredAt.Before(h[j].ExpiredAt)}
 
-func (h ExpiryHeap) Swap(i,j int) {}
+func (h ExpiryHeap) Swap(i,j int) {
+	h[i],h[j] = h[j], h[i]
+	h[i].Index = i
+	h[j].Index = j
+}
 
-func (h *ExpiryHeap) Push(val any){}
+func (h *ExpiryHeap) Push(val any){
+	item := val.(*ExpiryItem)
+	item.Index = len(*h)
+	*h = append(*h, item)
+}
 
-func (h *ExpiryHeap) Pop() any{return nil}
+func (h *ExpiryHeap) Pop() any{
+	old := *h
+	n := len(*h)
+	item := old[n-1]
+	old[n-1] = nil
+	item.Index = -1
+	*h = old[:n-1]
+
+	return item
+}
